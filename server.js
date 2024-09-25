e9140da85ca4bc8ee45d66c9915d7b5f8d5d0de3
GPuebla <gabrielpuebla16@gmail.com>
2024-09-25 06:25:18 -0300

    adding information

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Hello");
+  res.send("Hello Guy");
+});
+
+app.get('/bye', (req, res) => {
+  res.send("Bye my friend");
 });
  
 const port = 3000;
 
 app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
 });
